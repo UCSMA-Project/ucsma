3c34bf58fe232dd8e50ee6e211d730481fe00107
gg <g.p.gianacopoulos@gmail.com>
2018-08-18 00:35:45 -0700

    partial implementation of building kernel modules. todo list added

--- build.sh
+++ build.sh
@@ -61,3 +61,30 @@ make download
 
 # Build OpenWRT
 make -j $THREADS v=S
+
+# cd to repo root
+cd ..
+
+# prepare dir to build unlock kernel module
+BUILD_DIR=build_dir
+ATH_DRIVER_DIR=OpenWRT-14.07-JS9331/build_dir/target-mips_34kc_uClibc-0.9.33.2/linux-ar71xx_generic/linux-3.10.49/drivers/net/wireless/ath
+
+mkdir $BUILD_DIR
+cp -r $ATH_DRIVER_DIR build_dir
+mkdir $BUILD_DIR/toolchain
+cp OpenWRT-14.07-JS9331/bin/ar71xx/OpenWrt-Toolchain-ar71xx-for-mips_34kc-gcc-4.8-linaro_uClibc-0.9.33.2.tar.bz2 $BUILD_DIR/toolchain
+
+# todo: unzip toolchain
+# todo: store location of script in a var
+# todo: copy ucsma modules code to $BUILD_DIR/ath/ath9k
+# todo: compile modules
+# todo: compile packetspammer
+# todo: move various build products into one folder (e.g. modules, packetspammer, firmware images)
+# todo: consider whether raspi stuff should be included
+
+# make -C "/home/gg/documents/csc494/OpenWRT-14.07-JS9331/build_dir/target-mips_34kc_uClibc-0.9.33.2/linux-ar71xx_generic/linux-3.10.49/" \
+#        ARCH=mips \
+#        CROSS_COMPILE="/home/gg/documents/csc494/ucsma_build_dir/toolchain/OpenWrt-Toolchain-ar71xx-for-mips_34kc-gcc-4.8-linaro_uClibc-0.9.33.2/toolchain-mips_34kc_gcc-4.8-linaro_uClibc-0.9.33.2/bin//mips-openwrt-linux-" \
+#        M=/home/gg/documents/csc494/ucsma_build_dir/ath/ath9k     \
+#        modules
+
